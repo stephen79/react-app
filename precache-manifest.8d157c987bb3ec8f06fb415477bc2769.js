a3392a1ff24b8405e5e63c2801079d6f423454cc
Stephen <stephen.adsl@gmail.com>
2019-09-13 15:22:21 -0700

    Updates

---
+++ precache-manifest.8d157c987bb3ec8f06fb415477bc2769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218d8946f9188aea4a5c49de3ec359f7",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "702d369d4292b9bb1457",
+    "url": "/react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "50092569e8b63addb95c",
+    "url": "/react-app/static/js/2.7c314d9f.chunk.js"
+  },
+  {
+    "revision": "702d369d4292b9bb1457",
+    "url": "/react-app/static/js/main.b879abbd.chunk.js"
+  },
+  {
+    "revision": "3e23c132bbe58b8e36e4",
+    "url": "/react-app/static/js/runtime~main.06e48de0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
