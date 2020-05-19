987bd7368da182a09d6522e1714ed1bec78fdb8f
Stephen <stephen.adsl@gmail.com>
2020-05-18 18:54:18 -0700

    convert to typescript

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React SC 2020
+          Learn React SC 2020 Typescript
         </a>
       </header>
     </div>
